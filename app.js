b4e3134f4402f88784126a679697e540baa27478
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2016-12-12 10:09:43 +0100

    modification du dom intégralement en js

--- app.js
+++ app.js
@@ -2,20 +2,23 @@
 const ready = function() {
   // Loading
   const people = [
-    { firstname: "Landry", surname: "O'Hara" },
-    { firstname: "Sharon", surname: "O'Neil" },
-    { firstname: "Emerson", surname: "O'Connell" },
-    { firstname: "Roberto", surname: "O'Maley" }
+    { firstname: "Landry", surname: "O'Hara", age: "29", taille: "1m50" },
+    { firstname: "Sharon", surname: "O'Neil", age: "28", taille: "1m80" },
+    { firstname: "Emerson", surname: "O'Connell", age: "25", taille: "1m95" },
+    { firstname: "Roberto", surname: "O'Maley", age: "666", taille: "1m66" }
   ];
   var docfrag = document.createDocumentFragment();
-  //
   const tbody = document.querySelector("tbody");
-
+const thead = document.querySelector("thead");
+let tr = document.createElement("tr");
+tr.innerHTML = `<th>Prénom</th><th>Nom</th><th>Âge</th><th>Taille</th>`;
+docfrag.appendChild(tr);
   for(let i = 0; i < people.length; i++) {
     let tr = document.createElement("tr");
-    tr.innerHTML = `<td>${people[i].surname}</td><td>${people[i].firstname}</td>`;
+    tr.innerHTML = `<td>${people[i].surname}</td><td>${people[i].firstname}</td><td>${people[i].age}</td><td>${people[i].taille}</td>`;
     docfrag.appendChild(tr);
   }
+thead.appendChild(docfrag)
 tbody.appendChild(docfrag)
 
 }
